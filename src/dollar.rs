0728036dafb2cd06f97e8606add46b3ab49f59a0
tadashi-aikawa <syou.maman@gmail.com>
2023-09-10 21:26:00 +0900

    [1章] テストコードの場所をテスト対象に移す & 一部の重複削除

--- src/dollar.rs
+++ src/dollar.rs
@@ -4,8 +4,22 @@ pub struct Dollar {
 
 impl Dollar {
     pub fn new(amount: i32) -> Self {
-        Dollar { amount: 10 }
+        Dollar { amount }
     }
 
-    pub fn times(&self, multiplier: i32) {}
+    pub fn times(&mut self, multiplier: i32) {
+        self.amount *= multiplier
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiplication() {
+        let mut five = Dollar::new(5);
+        five.times(2);
+        assert_eq!(five.amount, 10);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-mod dollar;
-
-use crate::dollar::Dollar;
-use std::ops::Range;
-
 fn main() {
-    let xs: Range<i32> = -10..10;
-    let result: i32 = xs.filter(|x| x.is_positive()).map(|x| x * x).sum();
-    println!("{}", result);
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_multiplication() {
-        let five = Dollar::new(5);
-        five.times(2);
-        assert_eq!(five.amount, 10);
-    }
+    // Do nothing
 }
