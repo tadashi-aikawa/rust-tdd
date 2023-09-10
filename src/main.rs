f2cad484d103586cc5e9d97ef15e292b48ffa111
tadashi-aikawa <syou.maman@gmail.com>
2023-09-10 20:04:37 +0900

    [1章] とりあえず仮実装 & initial commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::ops::Range;
+
+fn main() {
+    let xs: Range<i32> = -10..10;
+    let result: i32 = xs.filter(|x| x.is_positive()).map(|x| x * x).sum();
+    println!("{}", result);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiplication() {
+        let five = Dollar::new(5);
+        five.times(2);
+        assert_eq!(five.amount, 10);
+    }
+}
