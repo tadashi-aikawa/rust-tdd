fba8adc77d13e69592821ead693868b2e86dca26
tadashi-aikawa <syou.maman@gmail.com>
2023-09-16 22:42:28 +0900

    [5章] フランのテストケースを追加

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -13,3 +13,11 @@ fn test_equality() {
     assert!(Dollar::new(5).eq(&Dollar::new(5)));
     assert!(!Dollar::new(5).eq(&Dollar::new(6)));
 }
+
+#[test]
+fn test_franc_multiplication() {
+    let mut five = Franc::new(5);
+
+    assert_eq!(five.times(2), Franc::new(10));
+    assert_eq!(five.times(3), Franc::new(15));
+}
